eb28c1f0313ea9b6a5d8930e47fbedfd16a4c99f
GUZN4ND4 <52776848+GUZN4ND4@users.noreply.github.com>
2019-07-11 16:33:58 +0800

    Add files via upload

---
+++ PR4NK3RS.sh
@@ -0,0 +1,55 @@
+clear
+figlet PR4NK3RS
+echo "=====================================================================
+                   
+   
+                       ╔════•ೋೋ•════╗ 
+                        °IG  guznanda_ 
+                       ╚════•ೋೋ•════╝
+=====================================================================" | lolcat
+
+sleep 1
+
+
+echo "[ 1 ] Spam Call
+[ 2 ] Spam Sms
+[ 3 ] Exit " | lolcat
+
+echo =============================="[GuzN4ND4]==============================
+Pilih nomor : " | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+figlet Sabar ya.... | lolcat
+pkg update 
+pkg upgrade 
+pkg install git
+git clone https://github.com/T14RB373T/FAKECALL
+cd FAKECALL
+bash call.sh
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+figlet SPAM SMS | lolcat
+pkg update 
+pkg upgrade
+pkg install git
+git clone https://github.com/KANG-NEWBIE/SpamSms
+cd SpamSms
+python main.py
+fi 
+
+if [ $nomor = 3 ] || [$nomor = 03 ]
+then 
+clear
+figlet E X I T | lolcat
+sleep 2
+clear
+figlet IG guznanda_ | lolcat
+sleep 2
+exit
+fi
